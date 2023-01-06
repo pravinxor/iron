fa34c312bd57de2be055914bd3c98f06f96b6c38
Pravin Ramana <pravinramana25@protonmail.ch>
2023-01-05 21:48:09 -0600

    Initial parser for quotes

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,58 @@
-enum Token<'a> {
-    Command(&'a str),
-    Argument(&'a str),
-}
-
-struct Tokens<'a> {
+pub struct Tokens<'a> {
     text: &'a str,
 }
 
+enum State {
+    Unquoted,
+    SingleQuotes,
+    DoubleQuotes,
+}
+
 impl<'a> Iterator for Tokens<'a> {
-    type Item = Token<'a>;
+    type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let mut end = 0;
+        let mut state = State::Unquoted;
+
+        let mut chars = self.text.chars();
+        while let Some(c) = chars.next() {
+            match c {
+                ' ' => {
+                    if end == 0 {
+                        self.text = &self.text[1..];
+                    } else {
+                        break;
+                    }
+                }
+                '\'' => match state {
+                    State::SingleQuotes => break,
+                    _ => state = State::SingleQuotes,
+                },
+                '"' => match state {
+                    State::DoubleQuotes => break,
+                    _ => state = State::DoubleQuotes,
+                },
+
+                _ => end += 1,
+            }
+        }
+        if end == 0 {
+            None
+        } else {
+            let res = Some(&self.text[..end]);
+            self.text = &self.text[end..];
+            res
+        }
+    }
+}
+
+pub trait TokenParser {
+    fn split_tokens(&self) -> Tokens;
+}
+
+impl TokenParser for str {
+    fn split_tokens(&self) -> Tokens {
+        Tokens { text: self }
     }
 }
