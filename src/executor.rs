0adf7a5443a3d3d7bfbdcf81607761754d43b1e7
Pravin Ramana <pravinramana25@protonmail.ch>
2023-01-07 17:11:25 -0600

    Implement pipes again

--- src/executor.rs
+++ src/executor.rs
@@ -3,6 +3,7 @@ where
     T: Iterator<Item = Result<crate::parser::Token<'a>, &'static str>>,
 {
     let mut child: Option<std::process::Command> = None;
+    let mut child_stdout = None;
     for token in tokens {
         let token = token?;
         match token {
@@ -11,7 +12,21 @@ where
                     child_command.arg(text);
                     Some(child_command)
                 } else {
-                    Some(std::process::Command::new(text))
+                    let mut child_command = std::process::Command::new(text);
+                    if let Some(stdout) = child_stdout.take() {
+                        child_command.stdin(stdout);
+                    }
+                    Some(child_command)
+                }
+            }
+            crate::parser::Token::Pipe => {
+                if child_stdout.is_some() {
+                    return Err("Unexpected additional pipe".into());
+                }
+                if let Some(mut child_command) = child.take() {
+                    child_command.stdout(std::process::Stdio::piped());
+                    let child_process = child_command.spawn()?;
+                    child_stdout = child_process.stdout
                 }
             }
             _ => return Err("Unhandled token".into()),
