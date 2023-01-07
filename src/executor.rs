02e4e6972b0f01061b7c4e063e61de6b5855bff1
Pravin Ramana <pravinramana25@protonmail.ch>
2023-01-07 17:35:08 -0600

    Extract command code into a separate function

--- src/executor.rs
+++ src/executor.rs
@@ -1,25 +1,34 @@
+fn command(
+    text: &str,
+    child_command: &mut Option<std::process::Command>,
+    child_process: &mut Option<std::process::Child>,
+) -> std::process::Command {
+    if let Some(mut command) = child_command.take() {
+        command.arg(text);
+        command
+    } else {
+        let mut child_command = std::process::Command::new(text);
+        if let Some(mut process) = child_process.take() {
+            if let Some(stdout) = process.stdout.take() {
+                child_command.stdin(stdout);
+            }
+        }
+        child_command
+    }
+}
+
 pub fn execute<'a, T>(tokens: &mut std::iter::Peekable<T>) -> Result<(), Box<dyn std::error::Error>>
 where
     T: Iterator<Item = Result<crate::parser::Token<'a>, &'static str>>,
 {
     let mut child_command: Option<std::process::Command> = None;
     let mut child_process: Option<std::process::Child> = None;
+    //let mut previous_token = None;
     for token in tokens {
         let token = token?;
         match token {
             crate::parser::Token::Text(text) => {
-                child_command = if let Some(mut command) = child_command.take() {
-                    command.arg(text);
-                    Some(command)
-                } else {
-                    let mut child_command = std::process::Command::new(text);
-                    if let Some(mut process) = child_process.take() {
-                        if let Some(stdout) = process.stdout.take() {
-                            child_command.stdin(stdout);
-                        }
-                    }
-                    Some(child_command)
-                }
+                child_command = Some(command(text, &mut child_command, &mut child_process));
             }
             crate::parser::Token::Pipe => {
                 if child_process.is_some() {
@@ -31,6 +40,8 @@ where
                     child_process = Some(process)
                 }
             }
+            crate::parser::Token::Redirect => {}
+
             _ => return Err("Unhandled token".into()),
         }
     }
